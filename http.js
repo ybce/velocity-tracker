f786f0cd2767b06d5648de7daaee23385f4f12b8
Youssef El Khalili <ylk@qlik.com>
2019-10-10 14:53:33 -0400

    initial commit

---
+++ http.js
@@ -0,0 +1,41 @@
+const _ = require('lodash')
+const request = require('request-promise-native')
+const log = require('./logger')
+const ua = require('./useragent')
+const USER_AGENT = ua.getUserAgent()
+
+class HttpUtil {
+  constructor (options = {}) {
+    this.request = options.request || request
+    this.logger = options.logger || { log }
+  }
+
+  async httpGet (bearerToken, url, customHeaders = {}) {
+    this.logger.log(`> GET ${url}`)
+    const res = await this.request({
+      method: 'GET',
+      url: url,
+      json: true,
+      resolveWithFullResponse: true,
+      simple: false,
+      headers: _.merge({
+        authorization: `token ${bearerToken}`,
+        'user-agent': USER_AGENT
+      }, customHeaders)
+    }).catch((err) => {
+      this.logger.log(`Error: ${err} on GET ${url}`)
+      this.logger.log(err)
+      throw err
+    })
+
+    this.logger.log(`< ${res.statusCode}`)
+    if (res.statusCode !== 200) {
+      this.logger.log(`Error: statusCode=${res.statusCode} on GET ${url}`)
+      this.logger.log(res.body)
+      throw new Error(_.isString(res.body) ? res.body : JSON.stringify(res.body))
+    }
+    return res.body
+  }
+}
+
+module.exports = HttpUtil
---
+++ index.js
@@ -0,0 +1,78 @@
+const args = require('commander')
+
+const packageInfo = require('./package.json')
+const HttpUtil = require('./http')
+const log = require('./logger')
+
+log(`${packageInfo.name} v${packageInfo.version}`)
+
+const httpUtil = new HttpUtil()
+
+const POINTS_LABELS = [
+  'Point: 0.5', 'Point: 1', 'Point: 2', 'Point: 3', 'Point: 5', 'Point: 8', 'Point: 13', 'Point: 21'
+]
+
+const POINTS_MAP = new Map([['Point: 0.5', 0.5], ['Point: 1', 1], ['Point: 2', 2], ['Point: 3', 3], ['Point: 5', 5], ['Point: 8', 8], ['Point: 13', 13], ['Point: 21', 21]])
+
+const projectColumn = 'https://github.com/orgs/qlik-trial/projects/25#column-6312145'
+
+args
+  .version(packageInfo.version)
+  .option('-t, --token [github_api_token]', 'Your GitHub API token', process.env.GITHUB_API_TOKEN)
+  .option('-o, --owner [repo_owner]', 'The GitHub repo owner - username or org name', process.env.REPO_OWNER)
+  .option('-r, --repo [repo_name]', 'The GitHub repo name', process.env.REPO_NAME)
+  .option('-m, --milestone [number]', 'Repo milestone number filter (from the GitHub URL)', process.env.REPO_MILESTONE)
+  .option('-l, --labels [label_list]', 'Comma-separated list of labels to filter on', process.env.REPO_LABELS)
+  .option('-i, --issues [issue_nums]', 'Comma-separated list of issue numbers to include', process.env.REPO_ISSUES)
+  .option('-p, --points', 'Flag to enable the points label on cards')
+  .option('-b, --beat [beat]', 'BEAT', process.env.BEAT)
+  .option('--project-column [url]', 'URL of GitHub project column to be printed', process.env.PROJECT_COLUMN_URL)
+  .parse(process.argv)
+
+// flag options can't have "values" so do the environment variable setting here
+if (process.env.INCLUDE_POINTS_LABELS) {
+  args.points = true
+}
+
+// verify that we have enough info to do something useful
+if (!args.token) {
+  console.error()
+  console.error('Error: Missing GitHub API token!')
+  args.help() // this automatically exits
+}
+// call async function from main without promise warnings
+// https://stackoverflow.com/questions/46515764/how-can-i-use-async-await-at-the-top-level
+
+(async () => {
+  const velocity = await processVelocity(args.token, projectColumn, args.beat)
+  console.log(velocity)
+})().catch(e => {
+  console.log('ERROR:')
+  console.log(e)
+  process.exit(1)
+})
+
+async function processVelocity (bearerToken, url = projectColumn, beat) {
+  console.log(typeof (beat))
+  const columnId = url.split('#column-')[1]
+  const cardsUrl = `https://api.github.com/projects/columns/${columnId}/cards`
+  const cardsBody = await httpUtil.httpGet(bearerToken, cardsUrl, { accept: 'application/vnd.github.inertia-preview+json' }
+  ).catch((err) => {
+    throw err
+  })
+
+  let count = 0
+  for (const card of cardsBody) {
+    if (card.content_url) {
+      const cardContent = await httpUtil.httpGet(bearerToken, card.content_url).catch((err) => { throw err })
+      if (cardContent.labels.find((label) => beat === label.name)) {
+        const pointLabel = cardContent.labels.find((label) => POINTS_LABELS.includes(label.name))
+        if (pointLabel) {
+          const cardPoints = POINTS_MAP.get(pointLabel.name)
+          count += cardPoints
+        }
+      }
+    }
+  }
+  return count
+}
